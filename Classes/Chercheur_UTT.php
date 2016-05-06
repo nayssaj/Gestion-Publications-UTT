c983f78cfd5b428677a43437cf1b74a2f710d47f
jglm <jglemercier@gmail.com>
2016-05-06 23:24:14 +0200

    Modifications syntaxe

--- Classes/Chercheur_UTT.php
+++ Classes/Chercheur_UTT.php
@@ -1,24 +1,23 @@
 <?php
-class Chercheur_UTT extends Auteur{
+    class Chercheur_UTT extends Auteur{
 
-private $login;
-private $MDP;
+        private $login;
+        private $MDP;
     
-function __construct ($nom,$deppartement,$login,$MDP){
+        function __construct ($nom, $departement, $login, $MDP){
     
-    //fonctionnalité de création du compte
+        //fonctionnalité de création du compte
+            parent::__construct($nom,$departement);
+            $this->universite = 'UTT';
+            $this->login = $login;
+            $this->MDP = $MDP;
     
-    parent::__construct($nom,$deppartement);
-    $this->universite = 'UTT';
-    $this->login = $login;
-    $this->MDP = $MDP;
-    
-}
+        }
 
-function getuniversite(){return ('UTT');}
+        function getUniversite(){return ('UTT');}
 
-function Ajout_Publication(){}
-function Modifier_Publication(){}
-function Afficher_Publications(){}
- 
-}
+        function ajoutPublication(){}
+        function modifierPublication(){}
+        function afficherPublications(){}
+    }
+?>
