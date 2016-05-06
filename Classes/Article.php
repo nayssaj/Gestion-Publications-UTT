23947aea0ee43dba9322201983b9d4dfa59979e9
jglm <jglemercier@gmail.com>
2016-05-06 23:15:12 +0200

    Modification syntaxe

--- Classes/Article.php
+++ Classes/Article.php
@@ -1,21 +1,21 @@
 <?php
+    class Article extends Publication{
 
-class Article extends Publication{
+        private $type; //peut être RI/RF/OS/AP
 
-    private $type; //peut être RI/RF/OS/AP
-
-    public function __construct($auteurs,$titre,$ref,$annee,$statut,$type){
-        parent::__construct($auteurs,$titre,$ref,$annee,$statut);
-        $this->type = $type;
-    }
-    
-    public function gettype(){return $this->type;}
-    
-    public function settype($type){
-        if($type=='RI' || $type=='RF' || $type=='OS' || $type=='AP'){
+        public function __construct($auteurs, $titre, $ref, $annee, $statut, $type){
+            parent::__construct($auteurs, $titre, $ref, $annee, $statut);
             $this->type = $type;
         }
+    
+        public function getType(){
+            return $this->type;
+        }
+    
+        public function setType($type){
+            if($type=='RI' || $type=='RF' || $type=='OS' || $type=='AP'){
+                $this->type = $type;
+            }
+        }
     }
-    
-    
-}
+?>
