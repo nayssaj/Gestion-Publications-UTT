cb1fc2d1df51ba95fb5758eb512a72851b321bf7
jglm <jglemercier@gmail.com>
2016-05-06 23:27:04 +0200

    Modifications syntaxe

--- Classes/Conference.php
+++ Classes/Conference.php
@@ -1,25 +1,23 @@
 <?php
-class Conference extends Publication{
+    class Conference extends Publication{
 
-    private $lieu;
-    private $type;
-    //put your code here
+        private $lieu;
+        private $type;
  
-    public function __construct($auteurs,$titre,$ref,$annee,$statut,$lieu,$type){
-        parent::__construct($auteurs,$titre,$ref,$annee,$statut);
-        $this->type = $type;
-        $this->lieu = $lieu;
-}
-
-    public function gettype(){return $this->type;}
-    public function settype($type){
-        if($type=='CI' || $type=='CF'){
+        public function __construct($auteurs, $titre, $ref, $annee, $statut, $lieu, $type){
+            parent::__construct($auteurs, $titre, $ref, $annee, $statut);
             $this->type = $type;
+            $this->lieu = $lieu;
         }
+
+        public function getType(){return $this->type;}
+        public function setType($type){
+            if($type=='CI' || $type=='CF'){
+                $this->type = $type;
+            }
+        }
+        
+        public function getLieu(){return $this->lieu;}
+        public function setLieu($lieu){$this->lieu = $lieu;}
     }
-    public function getlieu(){return $this->lieu;}
-    public function setlieu($lieu){$this->lieu = $lieu;}
-
-
-
-}
+?>
