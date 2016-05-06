7c1e34f03f02a744610a529a81f5cbb330e4e5b8
jglm <jglemercier@gmail.com>
2016-05-06 23:19:59 +0200

    Modifications syntaxe

--- Classes/Auteur.php
+++ Classes/Auteur.php
@@ -1,18 +1,19 @@
 <?php
-class Auteur {
+    class Auteur {
     
-private $nom;
-private $departement;
+        private $nom;
+        private $departement;
 
-function __construct ($nom,$departement){
-   $this->nom=$nom;
-   $this->departement=$departement;
-}
+        function __construct ($nom,$departement){
+            $this->nom = $nom;
+            $this->departement = $departement;
+        }
 
-function getnom(){return ($this->nom);}
-function setnom($nom){$this->nom = $nom;}
+        function getNom(){return ($this->nom);}
+        function setNom($nom){$this->nom = $nom;}
 
-function getdepartement(){return ($this->departement);}
-function setdepartement($departement){$this->departement = $departement;}
+        function getDepartement(){return ($this->departement);}
+        function setDepartement($departement){$this->departement = $departement;}
  
-}
+    }
+?>
