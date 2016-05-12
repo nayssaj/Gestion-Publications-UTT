f58e90e9af80b407804ef8f2cfd42931f1e73d50
jglm <jglemercier@gmail.com>
2016-05-12 17:35:29 +0200

    Crée classe singleton Database.php et méthode de connection

--- Classes/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-	try{
-		$bdd = new PDO("mysql:host=dev-isi.utt.fr;dbname=le_mercj;charset=utf8", "le_mercj", "ZDmh23Jd");
-	}
-	catch(Exception $e){
-		die('Erreur : ' . $e->getMessage());
-	}
-?>
