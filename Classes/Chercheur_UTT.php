d64f966a994f2e8745c9694de024dcdafaf15c2d
jglm <jglemercier@gmail.com>
2016-05-13 02:29:58 +0200

    Modifie visibilité méthodes

--- Classes/Chercheur_UTT.php
+++ Classes/Chercheur_UTT.php
@@ -1,23 +1,24 @@
 <?php
-    class Chercheur_UTT extends Chercheur{
 
-        private $login;
-        private $MDP;
+	require_once 'Database.php';	
+
+	class Chercheur_UTT extends Chercheur{
+
+        	private $login;
+        	private $MDP;
     
-        function __construct ($id, $nom, $prenom, $organisation, $equipe, $login, $MDP){
+        	public function __construct ($id, $nom, $prenom, $organisation, $equipe, $login, $MDP){
     
-        //fonctionnalité de création du compte
-            parent::__construct($$id, $nom, $prenom, 'UTT', $equipe);
-            $this->login = $login;
-            $this->MDP = $MDP;
-        }
+        	//fonctionnalité de création du compte
+            		parent::__construct($$id, $nom, $prenom, 'UTT', $equipe);
+            		$this->login = $login;
+            		$this->MDP = $MDP;
+        	}
 
-	function ajoutPublication($auteurs, $document, $titreArticle, $ref_Publication, $annee, $categorie, $statut){
-		require_once 'Database.php';	
-		$db = Database::getInstance();
+		public function ajoutPublication($auteurs, $document, $titreArticle, $ref_Publication, $annee, $categorie, $statut){
+			$db = Database::getInstance();
+		}
 
-	}
-
-        function modifierPublication(){}
-    }
+        	public function modifierPublication(){}
+    	}
 ?>
