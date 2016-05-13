0a7a6ad4f5a54070e2e612fa32d0ea30dc12c720
jglm <jglemercier@gmail.com>
2016-05-13 03:15:31 +0200

    ajoute methode ajoutPublication

--- Classes/Chercheur_UTT.php
+++ Classes/Chercheur_UTT.php
@@ -15,10 +15,24 @@
             		$this->MDP = $MDP;
         	}
 
-		public function ajoutPublication($auteurs, $document, $titreArticle, $ref_Publication, $annee, $categorie, $statut){
-			$db = Database::getInstance();
+		public function ajoutPublication($auteurs, $titre_article, $reference_publication, $annee, $categorie, $statut){
+			try{
+				$db = Database::getInstance();
+			}
+			catch(Exception $e){
+				die('Erreur : ' . $e->getMessage());	
+			}
+			$req = $bdd->prepare('INSERT INTO Publication(titre_article, reference_publication, annee, categorie, lieu, statut) VALUES(:titre_article, :reference_publication, :annee, :categorie, :lieu, :statut)');
+			$req->execute(array(
+				'titre_article' => $titre_article,
+				'reference_publication' => $reference_publication,
+				'annee' => $annee,
+				'categorie' => $categorie,
+				'lieu' => $lieu,
+				'statut' => $statut
+			));
+			echo 'jeu ajouté';
 		}
-
         	public function modifierPublication(){}
     	}
 ?>
