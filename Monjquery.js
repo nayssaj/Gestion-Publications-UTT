5d4fd77576282a1ce26a67afe735b8258866208b
leoschneider <leo.schneider@utt.fr>
2016-05-18 20:08:44 +0200

    vérification avec l'évenement mouseenter

--- Monjquery.js
+++ Monjquery.js
@@ -89,41 +89,28 @@ $('.submitjs').on('click', function () {//vérification à la validation de la p
   
   
   
-  $('submitnojs').on('mouseenter',function(){
+  $('.submitnojs').on('mouseenter',function(){
       var chercheErreur2 = 0;
     
-    //$('.Mondanger').hide();
-    //$('.Moninfo').hide();
-    //$('.Monsucces').hide(); //on cache les anciennes alertes pour les nouvelles
     $(document).find('input[name!="lieu"]').each(function(){
       if (! $(this).val()) { //le cas ou il y aurait des formulaires vides
           has_empty = true;
-          $('.Mondanger').text("Attention ! il semblerait que vous ayez oublié de tout remplir.");
-          $('.Mondanger').show();
           chercheErreur2 +=1;
-      }
-     /* var val = $(this).val();
-      var matches = val.match(/\d+/g);
-      if (matches !== null) {                                       //le cas ou il y aurait des nombres
-        $('.Mondanger').text("Attention ! il semblerait que vous avez ajouté des nombres dans les champs.");
-        $('.Mondanger').show();
-      }*/
-          
+      }   
     });
+    
     $(document).find('input[name ="organisation[]"]').each(function(){ //on cherche un auteur UTTien
-          if($('.Maorga').val() !== 'UTT'){
-          $('.Moninfo').text('Le formulaire ne peut pas être accepté si il n\'y a pas d\'auteur de l\'UTT');
-          $('.Moninfo').show();
-          chercheErreur2 +=1;
-          }
+      if($('.Maorga').val() !== 'UTT'){
+      chercheErreur2 +=1;
+      }
     });
     
     if(chercheErreur2 === 0){ //si le formulaire n'a pas de soucis alors on dévérouille la validation
-          $('.Monsucces').text("Le formulaire est rempli et est pret à être envoyé.");
-          $('.Monsucces').show();
           $('.submitnojs').prop('disabled', false);
     }
+    else{
+        $('.submitnojs').prop('disabled', true);
+    }
     
-    
-    $('.Maverification').slideDown();
+    //$('.Maverification').slideDown();
   });
