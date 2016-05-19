49189b6a5025f8c08c6b05586ff3e46795f43daa
leoschneider <leo.schneider@utt.fr>
2016-05-19 22:19:15 +0200

    elements administrateur(suite)

---
+++ Classes/Administrateur.php
@@ -0,0 +1,17 @@
+<?php
+
+	require_once 'Database.php';	
+	//S'assurer qu'il est nécéssaire de faire ce require
+	require_once 'Chercheur.php';
+
+Class Administrateur {
+    private $login;
+    private $mdp;
+    
+    function detectionCoherenceDonnees(){
+        
+    }
+    function statistiquesChercheurs(){
+        
+    }
+}
