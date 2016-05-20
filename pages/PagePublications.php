e5e1cce804cc6d721440e2025372611a52ac1ace
jglm <jglemercier@gmail.com>
2016-05-20 02:24:24 +0200

    corrige mise en forme

--- pages/PagePublications.php
+++ pages/PagePublications.php
@@ -181,13 +181,13 @@
             </div>
             <!-- /.row -->
             <div class="row">
-				<?php foreach($publicationsAuteur as $publication) :?>
-					<div class="col-lg-12">
+		<?php foreach($publicationsAuteur as $publication) :?>
+			<div class="col-lg-12">
                     	<!-- /.panel -->
                     		<div class="panel panel-default">
                         		<div class="panel-heading">
                             			<i class="fa fa-bar-chart-o fa-fw"></i>
-										<?php $publication->getTitre();?>
+						<?php $publication->getTitre();?>
                         		</div>
                         		<!-- /.panel-heading -->
                         		<div class="panel-body">
@@ -197,38 +197,38 @@
                                         			<table class="table table-bordered table-hover table-striped">
                                             			<thead>
                                                 			<tr>
-                                                    			<th>Auteurs</th>
-                                                    			<th>Ref</th>
-                                                    			<th>Annee</th>
+                                                    				<th>Auteurs</th>
+                                                    				<th>Ref</th>
+                                                    				<th>Annee</th>
                                               					<th>Statut</th>
                                                 			</tr>
                                             			</thead>
                                             			<tbody>
                                                 			<tr>
-                                                    			<td>
-																<?php 
-																	foreach($auteursPub as $auteur){ 
-																	echo $auteur->getNom() . '<br/>';
-																}?>
-																</td>
-                                                    			<td><?php echo $publication->getRef();?></td>
-                                                    			<td><?php echo $publication->getAnnee();?></td>
-																<td><?php echo $publication->getStatut();?></td>
+                                                    				<td>
+											<?php 
+												foreach($auteursPub as $auteur){ 
+													echo $auteur->getNom() . '<br/>';
+												}
+											?>
+										</td>
+                                                    				<td><?php echo $publication->getRef();?></td>
+                                                    				<td><?php echo $publication->getAnnee();?></td>
+										<td><?php echo $publication->getStatut();?></td>
                                                 			</tr>
                                             			</tbody>
                                         			</table>
                                     			</div>
-                                			</div>
-                                			<div class="col-lg-12">
+                                		</div>
+                                		<div class="col-lg-12">
                                     			<div id="morris-bar-chart"></div>
-                                			</div>
-                            			</div>
-                        			</div>
-                    			</div>
-                			</div>
+                                		</div>
+                            		</div>
+                        	</div>
+                    	</div>
+               	</div>
                	<?php endforeach; ?>
                 <!-- /.col-lg-8 -->
-                
                 <!-- /.col-lg-4 -->
             </div>
             <!-- /.row -->
