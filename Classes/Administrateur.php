286936f945b71824c2b9f8f68441585667982cdf
leoschneider <leo.schneider@utt.fr>
2016-05-23 14:56:01 +0200

    fonction retour de tous les utilisateurs

--- Classes/Administrateur.php
+++ Classes/Administrateur.php
@@ -8,10 +8,27 @@ Class Administrateur {
     private $login;
     private $mdp;
     
+    public function getUtilisateurs(){
+        //On cherche toutes les publications écrites par l'auteur
+            $reqUtilisateurs = 'SELECT * FROM Comptes WHERE statut=utilisateur';
+            $reponseUtilisateurs = $this->executerRequete($reqUtilisateurs);
+            while($donneesUtilisateurs = $reponseUtilisateurs->fetch()){
+                $reqNom = 'SELECT nom,prenom FROM Auteur WHERE Auteur.id = Comptes.auteur_id';
+                $reponseNom = $this->executerRequete($reqNom);
+                while($donneesNom = $reponseNom->fetch()){
+                    $Utilisateurs[] = new Chercheur_UTT($donneesUtilisateurs['auteur_id'], $donneesNom['nom'], $donneesNom['prenom'], $donneesUtilisateurs['equipe'],$donneesUtilisateurs['login'],$donneesUtilisateurs['mdp']); 
+                }
+            }
+            return $Utilisateurs;
+    }
+    
     function detectionCoherenceDonnees(){
         
     }
     function statistiquesChercheurs(){
+        $nomChercheur ='bobi';
+        $pointChercheur =4;
+        $points = array(nomChercheur,$pointChercheur);
         
     }
 }
