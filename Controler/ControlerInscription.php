7e2f157f74a2df2e1fd31b1636a1a92b0630cd14
jglm <jglemercier@gmail.com>
2016-05-25 01:55:44 +0200

    update path and remove authentification

--- Controler/ControlerInscription.php
+++ Controler/ControlerInscription.php
@@ -1,6 +1,7 @@
 <?php
-	require_once('../Classes/Publication.php');
-	require_once('../Classes/Chercheur.php');
+	require_once('Classes/Publication.php');
+	require_once('Classes/Chercheur.php');
+        require_once 'vue/Vue.php';
 	//require_once('../Vue/vue.php');
 
 	class ControlerInscription{
@@ -8,26 +9,11 @@
 		private $publication;
 
 		public function __construct(){
-			$this->publication = new Publication();
 		}
 
 		public function Inscription(){
-		//	$publication = $this->publication->getPublication($isBillet);
-		//	$vue = new Vue('Publication');
-		//	$vue->generer($publication);
+			$vue = new Vue('Inscription');
+			$vue->generer('Inscription');
 
-            if(isset($_SESSION["login"]) && isset($_SESSION["mdp"])){
-            }
-            else{if(isset($_POST[nom]) && isset($_POST[prenom]) && isset($_POST[equipe]) && isset($_POST[login]) && isset($_POST[mdp])){
-                    if(strlen($_POST[mdp])>6 ){ //ajouter les vérif d'existance login,nom et prenom 
-                    //créer les éléments dans la BDD
-                   // foreach ($_POST as $ele){
-                   //     echo $ele;
-                   //     echo ("<br/>");
-                   // }
-                    }
                 }
-            }
-            
-    }
-}
+        }
