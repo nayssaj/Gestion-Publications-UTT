462f0a0967b11a3eea56ef4f2547f4094476dae8
jglm <jglemercier@gmail.com>
2016-05-25 14:44:25 +0200

    renomme vue en Vue

--- Vue/Vue.php
+++ Vue/Vue.php
@@ -7,9 +7,13 @@
         private $titrePage;
         private $script;
 
-        public function __construct($action){
-            //Determinationi du nom du fichier vue a partir de l'action
-            $this->fichier = "vue/vue" . $action . ".php";
+        public function __construct($action, $controleur =""){
+            //Determinationi du nom du fichier vue a partir de l'action et du controleur
+            $fichier = "Vue/";
+            if ($controleur != ""){
+                $fichier = $fichier . $controleur . "/";
+            }
+            $this->fichier = $fichier . $action . ".php";
         }
 
         //Genere et affiche la vue
