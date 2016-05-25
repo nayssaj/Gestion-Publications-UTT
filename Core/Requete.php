f5a1821ebac825bdeed3246cc6ac19ab95be8102
jglm <jglemercier@gmail.com>
2016-05-25 11:36:11 +0200

    définit classe requet

---
+++ Core/Requete.php
@@ -0,0 +1,27 @@
+<?php
+    
+    class Requete{
+
+        //Param
+        private $parametres;
+
+        public function __construct($parametres){
+            $this->parametres = $parametres;
+        }
+
+        //Renvoi vrai si le parametre existe dans la requête
+        public function existeParametre($nom){
+            return (isset($this->paramtres[$nom]) && $this->parametres[$nom] != "");
+        }
+        
+        //Renvoi la valeur du parametre demandé
+        //Lève une exception si le parametre est introuvable
+        public function getParametres($nom){
+            if($this->existeParametre($nom)){
+                return $this->parametres[$nom];
+            }
+            else{
+                throw new Exception("Le parametre '$nom' absent de la requete");
+            }
+        }
+    }
