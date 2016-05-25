e1364e13784540b9447511812697e29004735933
jglm <jglemercier@gmail.com>
2016-05-25 11:35:19 +0200

    deplace Routeur dans core

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 'On');
 
-    require 'Controler/Routeur.php';
+    require 'Core/Routeur.php';
     
     $routeur = new Routeur();
     $routeur->routerRequete();
