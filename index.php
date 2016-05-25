834d97346805a7ff8250c2bf6e54b5e465622237
jglm <jglemercier@gmail.com>
2016-05-25 02:00:09 +0200

    définit index

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 'On');
-/*
-    require_once 'Controler/Routeur.php';
+
+    require 'Controler/Routeur.php';
     
     $routeur = new Routeur();
     $routeur->routerRequete();
- */
-    require_once 'vue/Vue.php';
+ 
+/*    require_once 'vue/Vue.php';
     require_once 'Classes/Chercheur.php';
     $michel = new Chercheur(3, 'michel', 'dupont', 'UTT', 'equipe');
-    $publications = $michel->getPublication();
 
-    $vue = new Vue("AjoutPublication");
-    $donneesSpecifiques = array('titrePage' => "Ajouter une publication");
+    $vue = new Vue("Erreur");
+    $donneesSpecifiques = array('titrePage' => "Wallah");
     $vue->generer($donneesSpecifiques);
+ */
