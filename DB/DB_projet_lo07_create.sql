0ea5ecaaf89e26cb72543fca8f3e8de37f5af647
jglm <jglemercier@gmail.com>
2016-05-30 17:48:43 +0200

    prise en compte des dates

--- DB/DB_projet_lo07_create.sql
+++ DB/DB_projet_lo07_create.sql
@@ -1,57 +1,99 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2016-05-12 13:06:13.74
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Lun 30 Mai 2016 à 17:43
+-- Version du serveur: 5.5.47-0ubuntu0.14.04.1
+-- Version de PHP: 5.5.9-1ubuntu4.16
 
--- tables
--- Table: Auteur
-CREATE TABLE Auteur (
-    id int NOT NULL,
-    organisation varchar(50) NOT NULL,
-    equipe varchar(25) NOT NULL,
-    nom varchar(25) NOT NULL,
-    prenom varchar(25) NOT NULL,
-    CONSTRAINT Auteur_pk PRIMARY KEY (id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- Table: Comptes
-CREATE TABLE Comptes (
-    Auteur_id int NULL,
-    statut varchar(25) NOT NULL,
-    login varchar(50) NOT NULL,
-    mdp varchar(50) NOT NULL,
-    CONSTRAINT Comptes_pk PRIMARY KEY (login)
-);
 
--- Table: Publication
-CREATE TABLE Publication (
-    id int NOT NULL,
-    titre_article varchar(25) NOT NULL,
-    reference_publication varchar(25) NOT NULL,
-    annee int NOT NULL,
-    categorie varchar(25) NOT NULL,
-    lieu varchar(25) NULL,
-    statut varchar(12) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
--- Table: redige
-CREATE TABLE redige (
-    Publication_id int NOT NULL,
-    Auteur_id int NOT NULL,
-    CONSTRAINT redige_pk PRIMARY KEY (Publication_id,Auteur_id)
-);
+--
+-- Base de données: `Gestion_Publication_UTT`
+--
 
--- foreign keys
--- Reference: Comptes_Auteur (table: Comptes)
-ALTER TABLE Comptes ADD CONSTRAINT Comptes_Auteur FOREIGN KEY Comptes_Auteur (Auteur_id)
-    REFERENCES Auteur (id);
+-- --------------------------------------------------------
 
--- Reference: redige_Auteur (table: redige)
-ALTER TABLE redige ADD CONSTRAINT redige_Auteur FOREIGN KEY redige_Auteur (Auteur_id)
-    REFERENCES Auteur (id);
+--
+-- Structure de la table `Auteur`
+--
 
--- Reference: redige_Publication (table: redige)
-ALTER TABLE redige ADD CONSTRAINT redige_Publication FOREIGN KEY redige_Publication (Publication_id)
-    REFERENCES Publication (id);
+CREATE TABLE IF NOT EXISTS `Auteur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `organisation` varchar(50) NOT NULL,
+  `equipe` varchar(25) NOT NULL,
+  `nom` varchar(25) NOT NULL,
+  `prenom` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=38 ;
 
--- End of file.
 
+--
+-- Structure de la table `Comptes`
+--
+
+CREATE TABLE IF NOT EXISTS `Comptes` (
+  `utilisateur_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(100) NOT NULL,
+  `mdp` varchar(50) NOT NULL,
+  PRIMARY KEY (`utilisateur_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+
+--
+-- Structure de la table `Publication`
+--
+
+CREATE TABLE IF NOT EXISTS `Publication` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre_article` varchar(500) NOT NULL,
+  `reference_publication` varchar(500) NOT NULL,
+  `annee` int(11) NOT NULL,
+  `categorie` varchar(25) NOT NULL,
+  `lieu` varchar(100) DEFAULT NULL,
+  `statut` varchar(20) NOT NULL,
+  `date_ajout` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+
+
+--
+-- Structure de la table `redige`
+--
+
+CREATE TABLE IF NOT EXISTS `redige` (
+  `Publication_id` int(11) NOT NULL,
+  `Auteur_id` int(11) NOT NULL,
+  PRIMARY KEY (`Publication_id`,`Auteur_id`),
+  KEY `redige_Auteur` (`Auteur_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `Comptes`
+--
+ALTER TABLE `Comptes`
+  ADD CONSTRAINT `Comptes_ibfk_1` FOREIGN KEY (`utilisateur_id`) REFERENCES `Auteur` (`id`);
+
+--
+-- Contraintes pour la table `redige`
+--
+ALTER TABLE `redige`
+  ADD CONSTRAINT `redige_Auteur` FOREIGN KEY (`Auteur_id`) REFERENCES `Auteur` (`id`),
+  ADD CONSTRAINT `redige_Publication` FOREIGN KEY (`Publication_id`) REFERENCES `Publication` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
