42f155f90245712f6a65823320d8922cee461e20
jglm <jglemercier@gmail.com>
2016-06-12 17:15:30 +0200

    administrateur ajoute désormais les utilisateur

--- Classes/Administrateur.php
+++ Classes/Administrateur.php
@@ -1,12 +1,9 @@
 <?php
 
-	require_once 'Database.php';	
-	//S'assurer qu'il est nécéssaire de faire ce require
-	require_once 'Chercheur.php';
+        require_once 'Core/Modele.php';
+	require_once 'Classes/Chercheur.php';
 
-Class Administrateur {
-    private $login;
-    private $mdp;
+Class Administrateur extends Modele{
     
     public function getUtilisateurs(){
         $reqUtilisateurs = 'SELECT * FROM Comptes WHERE statut=utilisateur';
@@ -29,10 +26,11 @@ Class Administrateur {
             $reqChercheur = 'SELECT * FROM Auteur ORDER BY id';
             $reponseChercheur = $this->executerRequete($reqChercheur);
             while($donneesChercheur = $reponseChercheur->fetch()){
-                $Chercheur[] = new Chercheur($donneesChercheur['id'], $donneesChercheur['nom'], $$donneesChercheur['prenom'],$donneesChercheur['organisation'] ,$donneesChercheur['equipe']); 
+                $Chercheur[] = new Chercheur($donneesChercheur['id'], $donneesChercheur['nom'], $donneesChercheur['prenom'],$donneesChercheur['organisation'] ,$donneesChercheur['equipe']); 
             }
             for($i=0;$i<=sizeof($Chercheur);$i++){
-                $getpubli =$Chercheur[$i]->getPublication();
+                $idChercheur = $Chercheur[$i]->getId();
+                $getpubli = $Chercheur[$i]->getPublications($idChercheur);
                 $nbpublié =0;$nbRI =0;$nbCI =0;$nbCF =0;
                 foreach($getpubli as $a){
                     $annees[] = $a->getAnnee();
@@ -44,9 +42,24 @@ Class Administrateur {
                 $nbpublication[$i] = sizeof($getpubli);
                 $anneemax[$i] = max($annees);
                 $anneemin[$i] = min($annees);
-            $pointsChercheur[$i] = $nbpublication[$i]/($anneemax[$i] - $anneemin[$i]) * ($nbpublié/$nbpublication) * (($nbCI + $nbRI)/$nbpublication) * (($nbCI+$nbRI)/$nbpublication);        
+                echo $nbpublication[$i];
+                echo $anneemax[$i];
+                echo $anneemin[$i];
+                echo $nbpublié;
+            $pointsChercheur[$i] = ($nbpublication[$i]/($anneemax[$i] - $anneemin[$i])) * ($nbpublié/$nbpublication[$i]) * (($nbCI + $nbRI)/$nbpublication[$i]) * (($nbCI+$nbRI)/$nbpublication[$i]);        
         $Résultat[i] = array($Chercheur[$i]->getNom(),$Chercheur[$i]->getPrenom(),$pointsChercheur[$i]);
         }
-        return $Résultat;
+        //return $Résultat;
+        print_r($Résultat);
+    }
+
+    public function ajouterUtilisateur(Chercheur_UTT $nouvelUtilisateur){
+        $sql = 'INSERT INTO Comptes VALUES(?, ?, ?);';
+        $this->executerRequete($sql, 
+            array(
+                $nouvelUtilisateur->getID(),
+                $nouvelUtilisateur->getLogin(),
+                $nouvelUtilisateur->getMdp(),
+            ));
     }
 }
