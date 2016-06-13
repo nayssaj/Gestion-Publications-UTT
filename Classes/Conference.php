ccbf09213dea18c998a6c50e00a4a91fed662c7f
jglm <jglemercier@gmail.com>
2016-06-13 09:45:41 +0200

    redefini classe publication

--- Classes/Conference.php
+++ Classes/Conference.php
@@ -1,15 +1,16 @@
 <?php
+    
+    require_once('Classes/Publication.php');
+
     class Conference extends Publication{
 
         private $lieu;
-        private $type;
  
-        public function __construct($id, $auteurs, $titre, $ref, $annee, $statut, $lieu, $type){
-            parent::__construct($id, $auteurs, $titre, $ref, $annee, $statut,$type);
+        public function __construct($id, $auteurs, $titre, $ref, $annee, $statut, $type, $lieu){
+            parent::__construct($id, $auteurs, $titre, $ref, $annee, $statut, $type);
             $this->lieu = $lieu;
         }
         
         public function getLieu(){return $this->lieu;}
         public function setLieu($lieu){$this->lieu = $lieu;}
     }
-?>
